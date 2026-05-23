b92bdbc86597008abbd7d6dec1707dcfeb5e6b39
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-05-23 20:21:22 +0530

    jokeApi

---
+++ jokeApp/script.js
@@ -0,0 +1,9 @@
+async function getNewJoke() {
+  const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous";
+  const response = await fetch(API_URL);
+
+  const data = await response.json();
+
+  document.getElementById("setup").innerText = data.joke || data.setup;
+  document.getElementById("delivery").innerText = data.dilevery || "";
+}
