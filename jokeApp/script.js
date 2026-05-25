e86477463637a101c43cebef04d090eb60c61158
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-05-25 18:50:41 +0530

    joke

--- jokeApp/script.js
+++ jokeApp/script.js
@@ -1,9 +1,17 @@
 async function getNewJoke() {
-  const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous";
-  const response = await fetch(API_URL);
 
-  const data = await response.json();
+    const API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
-  document.getElementById("setup").innerText = data.joke || data.setup;
-  document.getElementById("delivery").innerText = data.dilevery || "";
-}
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    document.getElementById("type").innerText = data.type;
+    document.getElementById("setup").innerText = data.setup;
+    document.getElementById("delivery").innerText = data.punchline;
+
+  
+  }
+
